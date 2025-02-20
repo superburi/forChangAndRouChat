56beb38a77ee10cd5afb0e991f4053b12e9a22f1
zhangyouhan <howardcyh@icloud.com>
2025-02-20 14:26:25 +0800

    初始化專案

---
+++ src/main/java/com/howard/forchangandrouchat/ForChangAndRouChatApplication.java
@@ -0,0 +1,13 @@
+package com.howard.forchangandrouchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForChangAndRouChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForChangAndRouChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/howard/forchangandrouchat/ForChangAndRouChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howard.forchangandrouchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForChangAndRouChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
