163c04a79b84858201bf6ae0ee65f707dfac705a
zhangyouhan <howardcyh@icloud.com>
2025-02-21 14:48:22 +0800

    測試cloud build4 -> 把專案生成的映像檔改放在自訂資料夾18

--- src/main/java/com/howard/forchangandrouchat/controller/testController.java
+++ src/main/java/com/howard/forchangandrouchat/controller/testController.java
@@ -8,7 +8,7 @@ public class testController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello World 123456789012345 !";
+        return "Hello World 1234567890123456 !";
     }
 
 }
